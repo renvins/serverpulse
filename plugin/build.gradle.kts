0333f84086933940b68efbbfbe327bfe0a312b51
Vincenzo R. <vincenzoreinaa05@gmail.com>
2025-03-29 14:12:23 +0100

    Remove unused library, use JDK implementation

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -17,7 +17,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.oshi:oshi-core:6.8.0")
     implementation("com.influxdb:influxdb-client-java:7.2.0")
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
 }
@@ -33,20 +32,7 @@ tasks.withType<ShadowJar> {
     archiveClassifier = "plugin"
     archiveVersion = "${rootProject.version}"
 
-    // --- Relocations ---
     relocate("com.influxdb", "$relocatePath.influxdb")
-    relocate("okhttp3", "$relocatePath.okhttp3")
-    relocate("okio", "$relocatePath.okio")
-    relocate("retrofit2", "$relocatePath.retrofit2")
-    relocate("com.squareup.moshi", "$relocatePath.moshi")
-    relocate("kotlin", "$relocatePath.kotlin")
-    relocate("kotlinx", "$relocatePath.kotlinx")
-    relocate("org.reactivestreams", "$relocatePath.reactivestreams")
-    relocate("oshi", "$relocatePath.oshi")
-    relocate("com.sun.jna", "$relocatePath.jna")
-    // other relocations needed
-
-    // --- End Relocations ---
 
     // --- Exclusions ---
     // Prevent Paper from giving errors about duplicate files
