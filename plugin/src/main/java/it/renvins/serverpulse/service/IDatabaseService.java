cfc3339d62e977f2f94aad0e91fce51b332e9aff
Vincenzo R. <vincenzoreinaa05@gmail.com>
2025-04-03 00:24:23 +0200

    Implement feature of InfluxDB retry

--- plugin/src/main/java/it/renvins/serverpulse/service/IDatabaseService.java
+++ plugin/src/main/java/it/renvins/serverpulse/service/IDatabaseService.java
@@ -6,6 +6,12 @@ import com.influxdb.client.WriteApi;
 public interface IDatabaseService extends Service {
 
     /**
+     * Returns the last known connection status. Does not perform a live check.
+     * @return true if the service believes it's connected, false otherwise.
+     */
+    boolean isConnected();
+
+    /**
      * Gets the configured InfluxDB client instance.
      *
      * @return The InfluxDBClient.
