afb061bf89eeecbfe6f8e5adea67013b6a3b8681
Vincenzo R. <vincenzoreinaa05@gmail.com>
2025-04-29 14:52:10 +0200

    Implement new methods on Platform

--- bukkit/src/main/java/it/renvins/serverpulse/bukkit/platform/BukkitPlatform.java
+++ bukkit/src/main/java/it/renvins/serverpulse/bukkit/platform/BukkitPlatform.java
@@ -1,7 +1,12 @@
 package it.renvins.serverpulse.bukkit.platform;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import it.renvins.serverpulse.api.data.WorldData;
 import it.renvins.serverpulse.common.platform.Platform;
 import it.renvins.serverpulse.bukkit.ServerPulseBukkit;
+import org.bukkit.World;
 
 public class BukkitPlatform implements Platform {
 
@@ -20,5 +25,27 @@ public class BukkitPlatform implements Platform {
     public void disable() {
         plugin.getServer().getScheduler().cancelTasks(plugin);
         plugin.getServer().getPluginManager().disablePlugin(plugin);
+    }
+
+    @Override
+    public boolean isPrimaryThread() {
+        return plugin.getServer().isPrimaryThread();
+    }
+
+    @Override
+    public int getOnlinePlayerCount() {
+        return plugin.getServer().getOnlinePlayers().size();
+    }
+
+    @Override
+    public Map<String, WorldData> getWorldsData() {
+        Map<String, WorldData> worldsData = new HashMap<>();
+        for (World world : plugin.getServer().getWorlds()) {
+            String name = world.getName();
+            WorldData data = new WorldData(world.getEntities().size(), world.getLoadedChunks().length);
+
+            worldsData.put(name, data);
+        }
+        return worldsData;
     }
 }
