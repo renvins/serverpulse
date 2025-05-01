be133e664787750077a681e602fa5c5ba604a10e
Vincenzo R. <vincenzoreinaa05@gmail.com>
2025-05-01 21:09:13 +0200

    Add new methods and default implementations to Platform and TaskScheduler interfaces

--- common/src/main/java/it/renvins/serverpulse/common/scheduler/TaskScheduler.java
+++ common/src/main/java/it/renvins/serverpulse/common/scheduler/TaskScheduler.java
@@ -2,12 +2,56 @@ package it.renvins.serverpulse.common.scheduler;
 
 public interface TaskScheduler {
 
-    void runSync(Runnable task);
+    /**
+     * Runs a task synchronously on the main thread.
+     *
+     * @param task The task to run
+     * @throws UnsupportedOperationException if the implementation does not support synchronous task execution
+     */
+    default void runSync(Runnable task) {
+        throw new UnsupportedOperationException("Synchronous task execution is not supported.");
+    }
+
+    /**
+     * Runs a task timer synchronously on the main thread.
+     *
+     * @param task The task to run
+     * @throws UnsupportedOperationException if the implementation does not support synchronous task execution
+     */
+    default Task runTaskTimer(Runnable task, long delayTicks, long periodTicks) {
+        throw new UnsupportedOperationException("Synchronous task execution is not supported.");
+    }
+
+    /**
+     * Runs a task later synchronously on the main thread.
+     *
+     * @param task The task to run
+     * @throws UnsupportedOperationException if the implementation does not support synchronous task execution
+     */
+    default Task runTaskLater(Runnable task, long delayTicks) {
+        throw new UnsupportedOperationException("Synchronous task execution is not supported.");
+    }
+
+    /**
+     * Runs a task asynchronously on a separate thread.
+     *
+     * @param task The task to run
+     */
     void runAsync(Runnable task);
 
-    Task runTaskTimer(Runnable task, long delayTicks, long periodTicks);
+    /**
+     * Runs a task timer asynchronously on a separate thread.
+     *
+     * @param task The task to run
+     * @return A Task object representing the task
+     */
     Task runTaskTimerAsync(Runnable task, long delayTicks, long periodTicks);
 
-    Task runTaskLater(Runnable task, long delayTicks);
+    /**
+     * Runs a task later asynchronously on a separate thread.
+     *
+     * @param task The task to run
+     * @return A Task object representing the task
+     */
     Task runTaskLaterAsync(Runnable task, long delayTicks);
 }
